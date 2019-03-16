b5fa826ac9564496ebd619b2e4bec630a8d3ffc3
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-16 16:11:32 +0100

    first commit

---
+++ src/main/java/pl/michalPajak/ShipsGame/ShipsGameApplication.java
@@ -0,0 +1,13 @@
+package pl.michalPajak.ShipsGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipsGameApplication.class, args);
+	}
+
+}
