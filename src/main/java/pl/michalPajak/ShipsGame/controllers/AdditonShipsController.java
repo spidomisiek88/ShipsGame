3cee22f4eb20b8844aa7a774b4562d286975b21d
spidomisiek88 <michal.pajak88@gmail.com>
2019-04-14 23:55:16 +0200

    addition ships

---
+++ src/main/java/pl/michalPajak/ShipsGame/controllers/AdditonShipsController.java
@@ -0,0 +1,4 @@
+package pl.michalPajak.ShipsGame.controllers;
+
+public class AdditonShipsController {
+}
---
+++ src/main/java/pl/michalPajak/ShipsGame/models/forms/AdditionShipsForm.java
@@ -0,0 +1,4 @@
+package pl.michalPajak.ShipsGame.models.forms;
+
+public class AdditionShipsForm {
+}
